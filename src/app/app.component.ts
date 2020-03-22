34f138ee99d8d84c962e34b0f850cbd929962003
MikeDxS <md_suarez04@hotmail.com>
2020-03-22 18:50:49 -0500

    Añadido manejo de la peticion http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'digitos-front';
+  data : string;
+  informacion : JSON;
+  constructor(){
+  }
+  enviar(url : string): void {
+    console.log('URL:',url);
+    fetch('http://localhost:5000/prediccion',{
+      method: 'POST',
+      body: JSON.stringify({
+        url: url
+      }),
+      headers: {'Content-Type': 'application/json'},
+    })
+    .then(function(response: Response) {
+      return response.json()
+    })
+    .then((datos)=>{
+      this.informacion = datos;
+    })
+  }
 }
